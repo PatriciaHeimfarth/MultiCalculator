d1d9eb4f7fea0beb9f7e6fd36a4964f48c23f8f0
Patti85 <pbeier24@googlemail.com>
2020-10-17 18:59:24 +0200

    first espresso test

---
+++ app/src/androidTest/java/de/softwarebuilding/multicalculator/InstrumentedTest.kt
@@ -0,0 +1,40 @@
+package de.softwarebuilding.multicalculator
+
+import android.content.Context
+import android.content.Intent
+import androidx.test.InstrumentationRegistry.getContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class InstrumentedTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(ScrollingActivity::class.java)
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("de.softwarebuilding.multicalculator", appContext.packageName)
+    }
+
+    @Test
+    fun messageShows() {
+
+        onView(withId(R.id.test)).perform(typeText("John"))
+
+    }
+
+
+}
